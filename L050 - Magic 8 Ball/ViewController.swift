aa3aa624eac1551a1f2a87d735b173cdf02a11b0
ArthurNikitin <Arthur.V.Nikitin@gmail.com>
2022-03-29 11:44:28 +0300

    Finish 58 Lesson

--- L050 - Magic 8 Ball/ViewController.swift
+++ L050 - Magic 8 Ball/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5"),
+        ]
 
-
-
+    
+    
+    @IBOutlet var imageAnswer: UIImageView!
+    
+    var ballAnwerNumber = 0
+    
+    @IBAction func askButton(_ sender: Any) {
+        
+        
+        ballAnwerNumber = Int.random(in: 0 ... 4)
+        imageAnswer.image = ballArray[ballAnwerNumber]
+        
+        print("Tap button")
+        print("Tap ballAnwerNumber \(ballAnwerNumber)")
+    }
+    
 }
 
